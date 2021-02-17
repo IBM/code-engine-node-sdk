7d102f7796bbd5eeb61dc275a74d2b4dcd11e34b
Tomas Roy <72228100+tomasroy-ibm@users.noreply.github.com>
2021-02-17 14:03:53 -0500

    feature: support for /project endpoint (GetKubeconfig) (#2)

--- example/example.js
+++ example/example.js
@@ -3,6 +3,8 @@
 const CodeEngineV1 = require('../dist/ibm-cloud-code-engine/v1'); // require('ibm-code-engine-sdk/ibm-cloud-code-engine/v1');
 const { IamAuthenticator } = require('../dist/auth'); // require('ibm-code-engine-sdk/auth');
 const k8s = require('@kubernetes/client-node');
+const axios = require('axios');
+const querystring = require('querystring');
 
 if (!process.env.CE_API_KEY || !process.env.CE_PROJECT_ID || !process.env.CE_PROJECT_REGION) {
   throw new Error(
@@ -24,26 +26,55 @@ const ceClient = new CodeEngineV1({
 });
 
 async function main() {
-  // Get tokens using the Authenticator.
-  const tokenResponse = await authenticator.tokenManager.requestToken();
+  // Use the http library to get an IAM Delegated Refresh Token
+  let iamResponse, delegatedRefreshToken;
+  try {
+    iamResponse = await axios.post('https://iam.cloud.ibm.com/identity/token', querystring.stringify({
+      'grant_type': 'urn:ibm:params:oauth:grant-type:apikey',
+      'apikey': process.env.CE_API_KEY,
+      'response_type': 'delegated_refresh_token',
+      'receiver_client_ids': 'ce',
+      'delegated_refresh_token_expiry': '3600'
+    }), {
+      'headers': {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    delegatedRefreshToken = iamResponse.data.delegated_refresh_token;
+  } catch(err) {
+    console.error('IAM POST /identity/token err:'+err);
+    process.exit(1);
+  }
 
   // Get Code Engine project config using the Code Engine client.
-  const configResponse = await ceClient.listKubeconfig({
-    refreshToken: tokenResponse.result.refresh_token,
-    id: process.env.CE_PROJECT_ID,
-  });
+  let configResponse;
+  try {
+     configResponse = await ceClient.getKubeconfig({
+      xDelegatedRefreshToken: delegatedRefreshToken,
+      id: process.env.CE_PROJECT_ID,
+    });
+  } catch(err) {
+    console.error('getKubeconfig err:'+err);
+    process.exit(1);
+  }
 
   // Setup Kubernetes client.
   const kubeConfig = new k8s.KubeConfig();
   kubeConfig.loadFromString(configResponse.result);
   const kubeClient = kubeConfig.makeApiClient(k8s.CoreV1Api);
+  const { namespace } = kubeConfig.getCurrentContextObject();
 
   // Get something from project.
-  const { namespace } = kubeConfig.getCurrentContextObject();
-  const configMapList = await kubeClient.listNamespacedConfigMap(namespace);
-  console.log(
-    `Project ${process.env.CE_PROJECT_ID} has ${configMapList.body.items.length} configmaps.`
-  );
+  try {
+    const configMapList = await kubeClient.listNamespacedConfigMap(namespace);
+    console.log(
+      `Project ${process.env.CE_PROJECT_ID} has ${configMapList.body.items.length} configmaps.`
+    );
+  } catch (err) {
+    console.error('listNamespacedConfigMap err:'+err);
+    process.exit(1);
+  }
+  
 }
 
 main();
